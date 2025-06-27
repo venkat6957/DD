49e8bfeede7bd213ada22e3e695ed42cd62b7e7d
kadali <kadali1229@gmail.com>
2025-06-27 16:46:34 +0530

    addded production url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://dd-production-97ad.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
